e6d022254d4dca44d7d312871dbefaede23ede2e
David Kecskemeti <dxdy421@gmail.com>
2022-04-04 08:19:26 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { unstable_batchedUpdates } from "react-dom";
+
+const getUserData = () => fetch("/user.json").then((res) => res.json());
 
 function App() {
+  const [name, setName] = useState<string>();
+  const [roles, setRoles] = useState<any>();
+  const [roleList, setRoleList] = useState<string[]>();
+
+  useEffect(() => {
+    console.log(`useEffect ${name} ${roles}`);
+
+    if (name) {
+      setRoleList(Object.keys(roles).filter((r: string) => roles[r]));
+    }
+  }, [name, roles]);
+
+  //React 16,17
+
+  //Done in batch
+  // const onLoadUser = () => {
+  //   setName("John");
+  //   setRoles({
+  //     editor: true,
+  //   });
+  // };
+
+  //Done in piece mod
+  const onLoadUser = async () => {
+    const data = await getUserData();
+    console.log(JSON.stringify(data));
+
+    //An another solution is to have a complex state
+    unstable_batchedUpdates(() => {
+      setName(data.name);
+      setRoles(data.roles);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Name: {JSON.stringify(name)}</div>
+      <div>Roles: {JSON.stringify(roles)}</div>
+      <div>Role List: {JSON.stringify(roleList)}</div>
+      <div>
+        <button onClick={() => onLoadUser()}>Load User</button>
+      </div>
     </div>
   );
 }
